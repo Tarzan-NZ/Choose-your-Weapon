4d993a79efee2ecaed247e36232f3ac4e5770c3c
Orion Gregg-schofield <noiroelite@windowslive.com>
2015-06-02 20:30:42 +1200

    add script files to most pages

---
+++ choose-your-weapon/js/js.js
@@ -0,0 +1,26 @@
+// record current page as a string
+// var pageValue = "1-1-1"
+// page value could be title
+
+// Listen to the button
+
+// Create function to save page title in local storage
+
+// var value = $.jstoragee.get("key");
+// if(!value){
+	// if not - load the data from the server 
+	// value = load_data_from_server()
+	// and save it
+	// $.jstorage.set("key" ,value);
+//}
+
+
+
+// on button press save page value
+$(document).ready(function(){
+
+
+});
+
+// Load page value and go to page
+
