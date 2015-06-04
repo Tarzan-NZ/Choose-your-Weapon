be899af6d9273693cf26730c858cc90af366e635
Orion Gregg-schofield <noiroelite@windowslive.com>
2015-06-04 21:11:54 +1200

    add classes to path page buttons and add some query to listen to those button

--- choose-your-weapon/js/js.js
+++ choose-your-weapon/js/js.js
@@ -2,9 +2,25 @@
 // var pageValue = "1-1-1"
 // page value could be title
 
-// Listen to the button
+// Set up our pageValue variable that we will use to save game.
+// Leave it blank so we can save player progress to local storage on save game button click
+var pageValue = '';
+
+// Listen to the save game button for clicks
+// and run the saveGame function
+
+$('.save-game').click(saveGame);
+
+// Listen to the resume button for clicks
+// and run the loadGame function
+$('.load-game').click(loadGame);
+
+// Listen to the delete button for clicks 
+// and run the deleteGame function
+// $('.delete-game').click(clearGame);
 
 // Create function to save page title in local storage
+if (localStorage.getItem) {};
 
 // var value = $.jstoragee.get("key");
 // if(!value){
@@ -14,13 +30,24 @@
 	// $.jstorage.set("key" ,value);
 //}
 
-
-
-// on button press save page value
 $(document).ready(function(){
 
 
 });
 
-// Load page value and go to page
+// Save Game button will have SaveGame function attached
+// that will grab current page title and save it to local storage
+// as 'pageValue'
 
+// Load Game button will need to first check
+// if there is a value stored in PageValue and if it is false
+// alert('No save game found, please start a new game')
+
+// Load page value and go to page
+// function attached to all load game buttons
+// so .onclick run loadGame function
+// which takes you to the page stored in local storage
+// as 'pageValue'
+
+// Delete Game button will simply clear
+// pageValue 
